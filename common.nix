05ac535c0dc9201dc1a6fcb4d6ff0744421102b6
Gian Lazzarini <1166579+gianlazz@users.noreply.github.com>
2026-05-14 23:28:29 -0700

    ~/.config/git/config as out traditional of store symlink

--- common.nix
+++ common.nix
@@ -23,35 +23,8 @@ in
   };
 
   # Git configuration
-  programs.git = {
-    enable = true;
-    settings = {
-      user.name = "Gian Lazzarini";
-      user.email = "1166579+gianlazz@users.noreply.github.com";
-      alias = {
-        co = "checkout";
-        br = "branch";
-        ci = "commit";
-        st = "status";
-      };
-      init.defaultBranch = "master";
-      pull.rebase = true;
-      push.autoSetupRemote = true;
-      diff = {
-        algorithm = "histogram";
-        colorMoved = "plain";
-        mnemonicPrefix = true;
-      };
-      commit.verbose = true;
-      column.ui = "auto";
-      branch.sort = "-committerdate";
-      tag.sort = "-version:refname";
-      rerere = {
-        enabled = true;
-        autoupdate = true;
-      };
-    };
-  };
+  home.file.".config/git/config".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/git/config";
 
   # Shell — direct symlink to repo (live-editable, tracked in git)
   home.file.".bashrc".source =
