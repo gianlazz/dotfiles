42473a704cff37c37afb612e296c97a5df5cf905
Gian Lazzarini <1166579+gianlazz@users.noreply.github.com>
2026-06-20 23:05:26 -0700

    whoops missed some

---
+++ nix/tmux.nix
@@ -0,0 +1,117 @@
+{ config, pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    mouse = true;
+    historyLimit = 50000;
+    escapeTime = 10;
+    keyMode = "vi";
+    baseIndex = 1;
+    terminal = "tmux-256color";
+    aggressiveResize = true;
+    focusEvents = true;
+    prefix = "C-Space";
+
+    plugins = [
+      {
+        plugin = pkgs.tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-strategy-vim 'session'
+        '';
+      }
+      {
+        plugin = pkgs.tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '15'
+        '';
+      }
+    ];
+
+    extraConfig = ''
+      # Secondary prefix
+      set -g prefix2 C-b
+
+      # Additional terminal settings
+      set -ag terminal-overrides ",*:RGB"
+      set -g escape-time 0
+      set -g renumber-windows on
+      set -g set-clipboard on
+      set -g allow-passthrough on
+      set -g detach-on-destroy off
+      set -g extended-keys on
+      set -g extended-keys-format csi-u
+
+      # Reload config
+      bind q source-file ~/.config/tmux/tmux.conf \; display "Configuration reloaded"
+
+      # Vi mode copy bindings
+      bind -T copy-mode-vi v send -X begin-selection
+      bind -T copy-mode-vi y send -X copy-selection-and-cancel
+
+      # Pane Controls
+      bind h split-window -v -c "#{pane_current_path}"
+      bind v split-window -h -c "#{pane_current_path}"
+      bind x kill-pane
+      bind -n C-M-Left select-pane -L
+      bind -n C-M-Right select-pane -R
+      bind -n C-M-Up select-pane -U
+      bind -n C-M-Down select-pane -D
+      bind -n C-M-S-Left resize-pane -L 5
+      bind -n C-M-S-Down resize-pane -D 5
+      bind -n C-M-S-Up resize-pane -U 5
+      bind -n C-M-S-Right resize-pane -R 5
+
+      # Window navigation
+      bind r command-prompt -I "#W" "rename-window -- '%%'"
+      bind c new-window -c "#{pane_current_path}"
+      bind k kill-window
+      bind -n M-1 select-window -t 1
+      bind -n M-2 select-window -t 2
+      bind -n M-3 select-window -t 3
+      bind -n M-4 select-window -t 4
+      bind -n M-5 select-window -t 5
+      bind -n M-6 select-window -t 6
+      bind -n M-7 select-window -t 7
+      bind -n M-8 select-window -t 8
+      bind -n M-9 select-window -t 9
+      bind -n M-Left select-window -t -1
+      bind -n M-Right select-window -t +1
+      bind -n M-S-Left swap-window -t -1 \; select-window -t -1
+      bind -n M-S-Right swap-window -t +1 \; select-window -t +1
+
+      # Session controls
+      bind R command-prompt -I "#S" "rename-session -- '%%'"
+      bind C new-session -c "#{pane_current_path}"
+      bind K kill-session
+      bind P switch-client -p
+      bind N switch-client -n
+      bind -n M-Up switch-client -p
+      bind -n M-Down switch-client -n
+
+      # Status bar
+      set -g status-position top
+      set -g status-interval 5
+      set -g status-left-length 30
+      set -g status-right-length 50
+      set -g window-status-separator ""
+      set -gw automatic-rename on
+      set -gw automatic-rename-format '#{b:pane_current_path}'
+
+      # Theme
+      set -g status-style "bg=default,fg=default"
+      set -g status-left "#[fg=black,bg=blue,bold] #S #[bg=default] "
+      set -g status-right "#[fg=blue]#{?pane_in_mode,COPY ,}#{?client_prefix,PREFIX ,}#{?window_zoomed_flag,ZOOM ,}#[fg=brightblack]#h "
+      set -g window-status-format "#[fg=brightblack] #I:#W "
+      set -g window-status-current-format "#[fg=blue,bold] #I:#W "
+      set -g pane-border-style "fg=brightblack"
+      set -g pane-active-border-style "fg=blue"
+      set -g message-style "bg=default,fg=blue"
+      set -g message-command-style "bg=default,fg=blue"
+      set -g mode-style "bg=blue,fg=black"
+      setw -g clock-mode-colour blue
+    '';
+  };
+}
